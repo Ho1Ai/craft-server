743f81e69f105a011541a3c990b7ff8205916ed6
Ho1Ai desktop <koekto33309@gmail.com>
2026-07-01 23:56:12 +0000

    working on database

--- src/pkg_list.sql
+++ src/pkg_list.sql
@@ -1,7 +1,24 @@
-create table pkg_list {
+create table pkg_list_actual (
     id BIGSERIAL PRIMARY KEY,
-    pkg_name UNIQUE TEXT,
-    pkg_build_type SMALLINT,
+    pkg_name TEXT UNIQUE NOT NULL,
+    pkg_version TEXT NOT NULL,
+    pkg_build_type SMALLINT DEFAULT 0,
     pkg_platform TEXT[],
-    pkg_path TEXT[],
-    }
+    platform_sensitive BOOLEAN,
+    pkg_dependencies_list TEXT[] DEFAULT '{}',
+    pkg_cdn_path TEXT[] DEFAULT '{}',
+    pkg_hash BYTEA,
+    pkg_size BIGINT,
+    pkg_last_update TIMESTAMP DEFAULT NOW(),
+    pkg_posted TIMESTAMP,
+    );
+
+/*
+    peers list table will be remade
+*/
+
+create table pkg_peers_list(
+    id BIGSERIAL PRIMARY KEY,
+    pkg_name TEXT REFERENCES pkg_list(pkg_name),
+    peers_list TEXT[],
+)
